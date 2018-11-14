603f1e8e555396cf6ff46d56fd32af44eb93b2bc
Steven Suzuki <suzukisteven@gmail.com>
2018-11-14 18:26:28 +0800

    completed all specs for list model

--- list_spec.rb
+++ list_spec.rb
@@ -4,5 +4,77 @@ require_relative "list"
 require_relative "task"
 
 describe List do
-  # Your specs here
+
+  Given(:title) { "Shopping List" } # Title to pass into the object as argument
+  Given(:description) {"some string"}
+  Given(:list) { List.new(title) } # Create a new instance of the object
+  Given(:task) { Task.new(description) } # A new task with a description
+
+  describe "#initialize" do
+    context "should create a new list with given title" do
+      When (:output){List.new(title)} # Create a new list with title 'Shopping List'
+      Then {expect(output).to be_a_kind_of(List)}
+    end
+
+    context "did not create a new list without given title" do
+      Then {expect{List.new}.to raise_error(ArgumentError)}
+    end
+  end
+
+  describe "#add_task" do
+    context "Check if tasks array has added a task object, i.e. not empty" do
+      When {list.add_task(task)} # Push the task into array of tasks
+      Then {expect(list.tasks.count).to eq(1)}
+    end
+  end
+
+  describe "#complete_task" do
+    context "Check if a task at a certain index in tasks array is complete(true) or not" do
+    # Given(:tasks)
+      When {
+        list.add_task(task)
+        list.complete_task(0)
+      }
+      Then {
+        list.tasks[0].complete? == true
+      }
+      # Then {expect(list.tasks[0].complete?).to eq(true)}
+    end
+  end
+
+  describe "#delete_task" do
+    context "Test if a single task has been removed from the tasks array" do
+      When {
+        list.add_task(task)
+        list.delete_task(0)
+      }
+      Then {
+        list.tasks[0] == nil
+      }
+    end
+  end
+
+  describe "#completed_tasks" do
+    context "Check tasks that are complete" do
+      When {
+        list.add_task(task)
+        list.complete_task(0)
+      }
+      Then {
+        list.completed_tasks[0].complete? == true
+      }
+    end
+  end
+
+  describe "#incomplete_tasks" do
+    context "Check tasks that are incomplete" do
+      When {
+        list.add_task(task)
+        list.incomplete_tasks
+      }
+      Then {
+        list.incomplete_tasks[0].complete? == false
+      }
+    end
+  end
 end
--- task_spec.rb
+++ task_spec.rb
@@ -12,7 +12,7 @@ describe Task do
       When (:output){Task.new(description)}
       Then {expect(output).to be_a_kind_of(Task)}
     end
-    
+
     context "requires one argument" do
       Then {expect{Task.new}.to raise_error(ArgumentError)} # noticed we used {Task.new} instead of (Task.new)? With () is to test the return value, {} is to test the behavior
     end
@@ -23,18 +23,18 @@ describe Task do
       Then {task.description == description}
     end
   end
-  
+
   describe "#complete?" do
     context "returns false for incomplete tasks" do
       Then {task.complete? == false}  # Rspec matcher version: expect(task.complete?).to be(false)
     end
-    
+
     context "returns true for completed tasks" do
       When {task.complete!}
       Then {task.complete? == true}
     end
   end
-  
+
   describe "#complete!" do
     context "changes the task from incomplete to completed" do
       When {task.complete!}
